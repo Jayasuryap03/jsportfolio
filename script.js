483473af625990455a1bf8024bb6664378cef030
Jayasuryap03 <116901812+Jayasuryap03@users.noreply.github.com>
2024-11-28 12:32:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contact Form Submission Alert
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for reaching out! I will respond soon.');
+  });
+  
